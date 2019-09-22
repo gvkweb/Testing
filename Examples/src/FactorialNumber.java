2a37f392c6099b8dffb8d48aff78e2d49ae5c4a7
Krishna <krishgani2@gmail.com>
2019-09-22 11:41:18 +0530

    factorial number programme

---
+++ Examples/src/FactorialNumber.java
@@ -0,0 +1,18 @@
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+
+	
+		int n =5;
+		int f=1;
+		for(int i=0;i<n;i++)
+		{
+			f = f*i; 
+			
+		}
+	
+	System.out.println("final Value");
+	}
+
+}
